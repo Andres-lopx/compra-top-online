acd5093d7d47feb3366db3df3473c01b48c21581
Andres Lopez <tendiredvalledupar@gmail.com>
2026-04-19 20:00:44 -0500

    configura layout global con Navbar e Inter font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Navbar from "@/components/Navbar"
-
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      <Navbar />
       <div className="flex flex-col items-center justify-center h-96 text-center">
         <h1 className="text-5xl font-bold text-black">CompraTopOnline</h1>
         <p className="mt-4 text-xl text-gray-400">
