2d5d3dad61a2e58d2540e861104f00f25dc6e85c
Andres Lopez <tendiredvalledupar@gmail.com>
2026-04-24 23:02:29 -0500

    fix prisma client con driverAdapters

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,16 @@
-import { PrismaClient } from "@prisma/client/edge"
+import { PrismaClient } from "@prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  var prisma: PrismaClient | undefined
 }
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 })
 
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter })
+export const prisma = global.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma
+  global.prisma = prisma
 }
